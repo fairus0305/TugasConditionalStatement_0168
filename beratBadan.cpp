12837e43230e1f3c1868504b647e1faf6c7617f1
fairus0305 <ruszezzy@gmail.com>
2026-03-09 18:51:17 +0700

    first commit

---
+++ beratBadan.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b)
+{
+    return (a + b) / 2;
+}
+
+string Status_lulus(float r)
+{
+    if (r >= 60)
+        return "lolos";
+    else
+        return "gagal";
+}
+
+int main()
+{
+    float nilBI, nilMT, rerata;
+    string status;
+
+    cout << "masukan nilai Bahasa Inggris= ";
+    cin >> nilBI;
+    cout << "masukan nilai Matematika= ";
+    cin >> nilMT;
+
+    rerata = Rata_rata(nilBI, nilMT);
+
+    cout << "status kelulusan =" << Status_lulus(rerata) << " ,dengan nilai rerata = " << rerata << endl;
+}
