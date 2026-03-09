8ec49494305bbb436573c1dd9a9975ac2b3e600d
fairus0305 <ruszezzy@gmail.com>
2026-03-09 19:16:35 +0700

    membuat fungsi untuk status berat badan

--- beratBadan.cpp
+++ beratBadan.cpp
@@ -6,3 +6,11 @@ float Rata_rata(float a, float b)
     return a / (b * b) ;
 }
 
+string BMI(float r)
+{
+    if (r <=25 )
+        return "berat badan normal";
+    else if(r <=18.5)
+        return "berat badan kurang";
+    else
+}
